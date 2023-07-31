701dbf8e26004dc6bc8d7f2d7c68ab387d06659e
Alena Deviatova <aedeviatova@yandex.ru>
2023-07-31 20:46:49 +0300

    add config to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { TrackModule } from './tracks/track.module';
 import { ArtistModule } from './artists/artist.module';
@@ -6,6 +7,13 @@ import { AlbumModule } from './albums/album.module';
 import { FavModule } from './favs/fav.module';
 
 @Module({
-  imports: [UsersModule, TrackModule, ArtistModule, AlbumModule, FavModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    TrackModule,
+    ArtistModule,
+    AlbumModule,
+    FavModule,
+  ],
 })
 export class AppModule {}
