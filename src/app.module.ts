834442ebe7e61346f57599f747d4c2f121b15394
Alena Deviatova <aedeviatova@yandex.ru>
2023-08-04 18:35:22 +0300

    add typeorm to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { TrackModule } from './tracks/track.module';
 import { ArtistModule } from './artists/artist.module';
 import { AlbumModule } from './albums/album.module';
 import { FavModule } from './favs/fav.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_DSN'),
+        autoLoadEntities: true,
+        synchronize: configService.get('ENV') === 'dev',
+      }),
+    }),
     UsersModule,
     TrackModule,
     ArtistModule,
